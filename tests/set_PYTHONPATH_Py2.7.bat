70eb1f6d3b7ab462de803b3af4c0bf37a43686c4
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2023-04-29 09:17:43 +0100

    Added remarks

--- tests/set_PYTHONPATH_Py2.7.bat
+++ tests/set_PYTHONPATH_Py2.7.bat
@@ -1,1 +1,6 @@
+REM Python 2 editable installs don't work very well (becuase of the /src/toml_tools structure?)
+REM SETUP Python 2 for venvs on windows
+REM py -2 -m ensurepip
+REM py -2 -m pip install virtualenv
+REM create venv as normal with virtualenv swapped for venv
 set PYTHONPATH=%PYTHONPATH%;C:\Users\James\Documents\Coding\repos\toml_tools\src
