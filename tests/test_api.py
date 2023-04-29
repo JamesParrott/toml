92c8c483b91abd8d3be25c03ae1bd1d1fdb710c6
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2023-04-29 11:22:45 +0100

    Refactored toml_test tests into their own file.  291/391 pass

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,7 +10,6 @@ import pytest
 import toml_tools
 from toml_tools.decoder import InlineTableDict
 
-from . import decoding_test
 
 
 TEST_STR = """
@@ -55,42 +54,8 @@ def test_bug_196():
     assert round_trip_bug_dict == bug_dict
     assert round_trip_bug_dict['x'] == bug_dict['x']
 
-# Hardcoded absolute path to run Iron Python tests from \src\
-TOML_TEST_DIR = r"C:\Users\James\Documents\Coding\repos\toml-test-master\toml-test-master\tests"
-
-VALID_DIR = os.path.join(TOML_TEST_DIR,"valid")
-
-def test_valid_tests():
-    for f in os.listdir(VALID_DIR):
-        if not f.endswith("toml"):
-            continue
-        with open(os.path.join(VALID_DIR, f)) as fh:
-            toml_tools.dumps(toml_tools.load(fh))
-
-def make_parses_correctly_test(toml_path):
-    def test_parses_toml_to_tagged_json_from():
-        json_path = os.path.splitext(toml_path)[0] + '.json'
-
-        if not os.path.exists(json_path):
-            raise FileExistsError('No json test file for toml file: %s' % toml_path)
-        
-        with open(toml_path,'rt') as toml_fh, open(json_path,'rt') as json_fh:
-            dict_ = toml_tools.load(toml_fh)
-            tagged = decoding_test.tag(dict_)
-            # jsoned = json.dumps(tagged)
-            expected = json.load(json_fh) #json_fh.read()
-            assert tagged == expected
-    return test_parses_toml_to_tagged_json_from
 
 
-TOML_GLOB = os.path.join(TOML_TEST_DIR,"valid", '**\*.toml')
-for file_path in glob.iglob(TOML_GLOB):
-    test_name = os.path.splitext(os.path.basename(file_path))[0]
-    test_func_name = 'test_toml_test_valid_%s' % test_name
-
-    # It would be cleaner to setattr these test functions as class methods, 
-    # but ironpython-pytest doesn't look for class methods as yet.
-    globals()[test_func_name] = make_parses_correctly_test(file_path)
 
 
 def test_circular_ref():
@@ -200,16 +165,8 @@ def test_decimal():
     assert Decimal(o["a"]).quantize(PLACES) == d["a"].quantize(PLACES)
 
 
-def test_invalid_tests():
-    invalid_dir = os.path.join(TOML_TEST_DIR,"invalid")
-    for f in os.listdir(invalid_dir):
-        if not f.endswith("toml"):
-            continue
-        with pytest.raises(toml_tools.TomlDecodeError):
-            with open(os.path.join(invalid_dir, f)) as fh:
-                toml_tools.load(fh)
 
-
+    
 def test_exceptions():
     with pytest.raises(TypeError):
         toml_tools.loads(2)
